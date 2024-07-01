1c583484b69eeac176032e68f4cd51b0bc6084d6
Bruno Carlos <brunocarlos.souza@hotmail.com>
2024-06-30 22:47:53 -0300

    trabalhando nas APIs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFCore.Dominio;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,18 +12,36 @@ namespace EFCore.WebAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+
+        public readonly HeroiContext _context;
+        public ValuesController(HeroiContext context)
         {
-            return new string[] { "value1", "value2" };
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet("filtro/{nome}")]
+        public ActionResult GetFiltro(string nome)
+        {
+            var listHeroi = _context.Herois
+                            .Where(h => h.Nome.Contains(nome))
+                            .ToList();
+            //var listHeroi = (from heroi in _context.Herois 
+            //                 where heroi.Nome.Contains(nome)
+            //                 select heroi).ToList();
+            return Ok(listHeroi);
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [HttpGet("{nameHero}")]
+        public ActionResult Get(string nameHero)
         {
-            return "value";
+            var heroi = new Heroi { Nome = nameHero};
+         
+            _context.Herois.Add(heroi);
+            _context.SaveChanges();
+         
+            return Ok();
         }
 
         // POST api/values
